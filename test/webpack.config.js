942e1df7024365a050753f0c5885611e62fe1a5e
Nate Abele <nate@radify.io>
2017-10-19 14:48:57 -0400

    Misc. cleanup (#4)

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -2,6 +2,6 @@ const merge = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = merge.smart(require('../webpack.config'), {
-  target: 'node', // webpack should compile node compatible code
+  target: 'node',
   externals: [nodeExternals()]
 });
