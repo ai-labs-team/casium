15a0eca50dc5d983a62831f5177560a80acd07bc
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-10-26 10:37:32 -0400

    testing changes

--- src/commands/browser.ts
+++ src/commands/browser.ts
@@ -2,6 +2,11 @@ import { is } from 'ramda';
 import Message from '../message';
 import { isEmittable } from '../util';
 
+/**
+* PushHistory, ReplaceHistory, and Back classes manage the browser history.
+* Additionally, PushHistory and ReplaceHistory check if the path is a string
+**/
+
 export class ReplaceHistory extends Message {
   public static expects = { path: is(String) };
 }
@@ -10,8 +15,11 @@ export class PushHistory extends Message {
   public static expects = { path: is(String) };
 }
 
-export class Back extends Message {
-}
+export class Back extends Message {}
+
+/**
+*
+**/
 
 export class Timeout extends Message {
   public static expects = { result: isEmittable, timeout: is(Number) };
