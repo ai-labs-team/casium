fe7ccec4809664f93f16527c212bc1c7ef4753e2
Peter Chapman <pnchappy92@gmail.com>
2017-12-08 13:43:23 -0500

    withCrednentials optional (#19)

--- src/effects/http.ts
+++ src/effects/http.ts
@@ -4,8 +4,8 @@ import { Request } from '../commands/http';
 import { constructMessage } from '../util';
 
 export default new Map([
-  [Request, ({ method, url, data, params, headers, result, error, always }, dispatch) => {
-    axios({ method, url, data, params, headers, withCredentials: true })
+  [Request, ({ method, url, data, params, headers, result, error, always, withCredentials = true }, dispatch) => {
+    axios({ method, url, data, params, headers, withCredentials })
       .then(pipe(constructMessage(result), dispatch))
       .catch(pipe(constructMessage(error), dispatch))
       .then(always && pipe(constructMessage(always), dispatch) || identity);
