7b0461bdf8cf928ca15b1062a567c96e3ed3880f
Peter Chapman <peterchappy@users.noreply.github.com>
2018-02-26 16:50:48 -0500

    checks for test

--- src/effects/browser.ts
+++ src/effects/browser.ts
@@ -3,12 +3,13 @@ import { pipe } from 'ramda';
 import { Back, PushHistory, ReplaceHistory, Timeout } from '../commands/browser';
 import Message, { MessageConstructor } from '../message';
 
-export const history = typeof window === 'undefined' ? History.createMemoryHistory() : History.createBrowserHistory();
+export const history = typeof window === 'undefined' || process.env.NODE_ENV === 'test' ?
+  History.createMemoryHistory() : History.createBrowserHistory();
 
 export default new Map<MessageConstructor, (data: any, dispatch: any) => any>([
   [PushHistory, ({ path, state }) => history.push(path, state || {})],
   [ReplaceHistory, ({ path, state }) => history.replace(path, state || {})],
-  [Back, ({ state }) => history.goBack(state)],
+  [Back, () => history.goBack()],
   [Timeout, ({ result, timeout }, dispatch) => setTimeout(
     () => pipe(Message.construct(result), dispatch)({}),
     timeout
