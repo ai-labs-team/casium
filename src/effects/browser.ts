5ad3ace2f16ba0a7e14578f573ee4b6785929849
Peter Chapman <peterchappy@users.noreply.github.com>
2018-02-26 12:39:27 -0500

    syncHistoryWithState

--- src/effects/browser.ts
+++ src/effects/browser.ts
@@ -5,6 +5,54 @@ import Message, { MessageConstructor } from '../message';
 
 export const history = typeof window === 'undefined' ? History.createMemoryHistory() : History.createBrowserHistory();
 
+export const syncHistoryWithState = history => {
+
+  // const delagate = 'route';
+  // const locationPath = 'location';
+
+  // const env = environment({ effects, dispatcher });
+  // const stateMgr = intercept(env.stateManager)();
+
+  const getLocationInStore = () => stateMgr.get({ path: [delagate, locationPath] });
+
+  const handleLocationChange = location => {
+    stateMgr.set({ location }, { path: [delagate] });
+  };
+
+  history.listen(handleLocationChange);
+
+  const unsubscribeFromHistory = history.listen(handleLocationChange);
+
+  return ({
+    ...history,
+
+    listen(listener) {
+      let lastPublishedLocation = getLocationInStore(true);
+      let unsubscribed = false;
+
+      const unsubscribeFromStore = stateMgr.subscribe(() => {
+        const currentLocation = getLocationInStore(true);
+        if (currentLocation === lastPublishedLocation) {
+          return;
+        }
+        lastPublishedLocation = currentLocation;
+        if (!unsubscribed) {
+          listener(lastPublishedLocation);
+        }
+      });
+
+      return () => {
+        unsubscribed = true;
+        unsubscribeFromStore();
+      };
+    },
+
+    unsubscribe() {
+      unsubscribeFromHistory();
+    },
+  });
+};
+
 export default new Map<MessageConstructor, (data: any, dispatch: any) => any>([
   [PushHistory, ({ path, state }) => history.push(path, state || {})],
   [ReplaceHistory, ({ path, state }) => history.replace(path, state || {})],
