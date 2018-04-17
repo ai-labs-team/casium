a7e38f2f76e1e6fa6942380e7dbdcd4e7184b2e6
Peter Chapman <peterchappy@users.noreply.github.com>
2018-04-17 10:47:03 -0400

    componenetDidCatch

--- src/view_wrapper.tsx
+++ src/view_wrapper.tsx
@@ -94,7 +94,7 @@ export default class ViewWrapper<M> extends React.Component<ViewWrapperProps<M>,
     this.execContext.destroy();
   }
 
-  public unstable_handleError(e) {
+  public componentDidCatch(e) {
     // tslint:disable-next-line:no-console
     console.error('Failed to compile React component\n', e);
     this.setState({ componentError: e });
